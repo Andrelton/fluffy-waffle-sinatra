9117188365de1aa7c72dec9a9d4f0d4e3e11803f
Andrelton <eric.saxman@gmail.com>
2015-12-03 21:30:39 -0800

    refactor various files for readability

--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
 require File.expand_path("../config/environment.rb", __FILE__)
-# set :root, File.dirname(__File__)
 run Sinatra::Application
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 require "pathname"
-APP_ROOT = Pathname.new(File.expand_path("../../", __FILE__))
-APP_NAME = APP_ROOT.basename.to_s
+APP_ROOT = File.expand_path("../../", __FILE__)
+APP_NAME = Pathname.new(APP_ROOT).basename.to_s
 
 require "bundler"
 Bundler.require
 
-set :root, File.expand_path("../../", __FILE__)
+set :root, APP_ROOT
 set :database, { adapter: "sqlite3", database: "#{APP_NAME}.sqlite3"}
 
 require File.expand_path("../../app.rb", __FILE__)
